227717c9e66025e7d8dd93d084277d715872c50e
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-05-30 02:11:30 +0700

    обновляем тесты, теперь функция может выбрасывать ошибку

--- main_test.go
+++ main_test.go
@@ -7,30 +7,45 @@ func Test_convertArshinToM(t *testing.T) {
 		n float64
 	}
 	tests := []struct {
-		name string
-		args args
-		want float64
+		name    string
+		args    args
+		want    float64
+		wantErr bool
 	}{
 		{
-			name: "0 аршин равен 0 метрам",
-			args: args{n: 0},
-			want: 0,
+			name:    "0 аршин равен 0 метрам",
+			args:    args{n: 0},
+			want:    0,
+			wantErr: false,
 		},
 		{
-			name: "1 аршин равен 0.71 метрам",
-			args: args{n: 1},
-			want: 0.71,
+			name:    "1 аршин равен 0.71 метрам",
+			args:    args{n: 1},
+			want:    0.71,
+			wantErr: false,
 		},
 		{
-			name: "2 аршина равно 1.42 метра",
-			args: args{n: 2},
-			want: 1.42,
+			name:    "2 аршина равно 1.42 метра",
+			args:    args{n: 2},
+			want:    1.42,
+			wantErr: false,
+		},
+		{
+			name:    "-1 аршин нельзя конвертировать",
+			args:    args{n: -1},
+			want:    0,
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := convertArshinToM(tt.args.n); got != tt.want {
-				t.Errorf("convertArshinToM() = %v, want %v", got, tt.want)
+			got, err := convertArshinToM(tt.args.n)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("convertArshinToM() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("convertArshinToM() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
