c0dc8aa6192220303249ef79a9d7f6113eac961d
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-05-30 02:32:51 +0700

    добавляем простой консольный интерфейс

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"universal-converter/converters"
+)
+
+func main() {
+	var n float64
+	fmt.Println("Введите аршины")
+	_, _ = fmt.Scanf("%f", &n)
+	m, err := converters.ConvertArshinToM(n)
+	if err != nil {
+		fmt.Println("не удалось перевести аршины в метры err:", err)
+		return
+	}
+	fmt.Printf("%.2f аршин равно %.2f метров", n, m)
+}
