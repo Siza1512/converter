eea3c60ceac86556cc7db75629e10a4108f3dea9
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-05-30 02:19:30 +0700

    исправляем код функции

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ var negativeNumberNotSupport = errors.New("negative number not support")
 
 func convertArshinToM(n float64) (float64, error) {
 	if n < 0 {
-		return 0, errors.New("error")
+		return 0, negativeNumberNotSupport
 	}
 	return n * 0.71, nil
 }
