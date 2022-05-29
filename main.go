30fe30d857cd044f7573ea256a87afd3719b801c
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-05-30 02:28:22 +0700

    удаляем не нужный код

--- main.go
+++ main.go
@@ -1,12 +1,1 @@
 package main
-
-import "errors"
-
-var negativeNumberNotSupport = errors.New("negative number not support")
-
-func convertArshinToM(n float64) (float64, error) {
-	if n < 0 {
-		return 0, negativeNumberNotSupport
-	}
-	return n * 0.71, nil
-}
